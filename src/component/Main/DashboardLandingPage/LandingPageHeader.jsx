a17d9d75535f615b2d962c135d560820b1f87b2e
nimurr <nimurnerob404@gmail.com>
2025-08-26 09:33:25 +0600

    add LandingPageHeader component and integrate it into DashboardHomePage

---
+++ src/component/Main/DashboardLandingPage/LandingPageHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPageHeader = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-semibold py-5 px-3"> header </h1>
+        </div>
+    );
+}
+
+export default LandingPageHeader;
--- src/page/DashboardHomePage/DashboardHomePage.jsx
+++ src/page/DashboardHomePage/DashboardHomePage.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import LandingPageHeader from '../../component/Main/DashboardLandingPage/LandingPageHeader';
 
 const DashboardHomePage = () => {
     return (
         <div>
+            <LandingPageHeader />
             <h1 className="text-4xl font-semibold py-5 px-3">Dashboard </h1>
         </div>
     );
