c2803c6eb65819904632835ef8f48a8304372d74
nimurr <nimurnerob404@gmail.com>
2025-09-01 10:23:17 +0600

    update

--- src/page/DashboardHomePage/HeroSection.jsx
+++ src/page/DashboardHomePage/HeroSection.jsx
@@ -7,13 +7,13 @@ const HeroSection = () => {
             <div className='absolute w-full h-[70vh] z-[1] rounded-xl bg-[#f3f5e9]'></div>
 
             {/* Content */}
-            <div className="text-center py-20 z-[999] relative">
+            <div className="text-center pt-20 pb-10 z-[999] relative px-5">
                 <h2 className='text-5xl text-gray-600 font-bold'>Canada's Premier AI-Driven Service Platform</h2>
                 <p className='text-xl my-5'>Experience the future of professional services with intelligent matching, augmented reality quoting, and guaranteed transparency.</p>
                 {/* <button className='px-8 py-3 bg-[#ffff01] text-black rounded-lg'>Experience SureServ</button> */}
             </div>
 
-            <div className="w-5/6 mx-auto z-[999] relative">
+            <div className="md:w-5/6 mx-auto z-[999] relative">
                 <video className="rounded-xl w-full" controls autoPlay src="https://res.cloudinary.com/nerob/video/upload/v1756700080/sureserv-video_KpvBhmla_lpdn62.mp4"></video>
             </div>
         </section>
