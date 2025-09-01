dd84c0b4e17a34245ee2a6ecbae60fef50505f86
nimurr <nimurnerob404@gmail.com>
2025-09-01 10:25:13 +0600

    update

--- src/page/DashboardHomePage/HeroSection.jsx
+++ src/page/DashboardHomePage/HeroSection.jsx
@@ -4,12 +4,12 @@ const HeroSection = () => {
     return (
         <section id='home' className='relative my-10'>
             {/* Background Div */}
-            <div className='absolute w-full h-[70vh] z-[1] rounded-xl bg-[#f3f5e9]'></div>
+            <div className='absolute w-full md:h-[70vh] h-full z-[1] rounded-xl bg-[#f3f5e9]'></div>
 
             {/* Content */}
             <div className="text-center pt-20 pb-10 z-[999] relative px-5">
-                <h2 className='text-5xl text-gray-600 font-bold'>Canada's Premier AI-Driven Service Platform</h2>
-                <p className='text-xl my-5'>Experience the future of professional services with intelligent matching, augmented reality quoting, and guaranteed transparency.</p>
+                <h2 className='md:text-5xl text-3xl text-gray-600 font-bold'>Canada's Premier AI-Driven Service Platform</h2>
+                <p className='md:text-xl my-5'>Experience the future of professional services with intelligent matching, augmented reality quoting, and guaranteed transparency.</p>
                 {/* <button className='px-8 py-3 bg-[#ffff01] text-black rounded-lg'>Experience SureServ</button> */}
             </div>
 
